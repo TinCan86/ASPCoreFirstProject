9d304583daae434b13ace14b540f3159c857b937
Robert Hansson <roberthanssonr@gmail.com>
2016-11-03 14:40:03 +0100

    Done Updates to ModelChapter on PluralSight

--- OdeToFood/src/OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/src/OdeToFood/Controllers/AboutController.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
+    [Route("company/[controller]/[action]")]
     public class AboutController
     {
         public string Phone()
@@ -12,7 +14,7 @@ namespace OdeToFood.Controllers
             return "1+555-555-5555";
         }
 
-        public string Adress()
+        public string Address()
         {
             return "Sweden";
         }
