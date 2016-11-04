e7d23bb43838b82925f8f35181d5e601307bd6d8
Robert Hansson <roberthanssonr@gmail.com>
2016-11-04 12:24:58 +0100

    Added DbContext class, started with the EF layout.

---
+++ OdeToFood/src/OdeToFood/Entites/OdeToFoodDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OdeToFood.Entites
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+
+        public OdeToFoodDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
